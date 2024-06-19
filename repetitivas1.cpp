673df45931973cf6cb54b73ae83ef1abada58ff1
rpinor <rpinor@unjbg.edu.pe>
2024-06-19 05:52:49 -0500

    Actualiza repetitivas1.cpp de estructuras repetitivas: Agrega Clear Screen

--- repetitivas1.cpp
+++ repetitivas1.cpp
@@ -1,14 +1,25 @@
 //PINO ROJAS Raul Farid Banchello//
 //Contraseña1.cpp//
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
 using namespace std;
 int main (){
+    int i;
     string clave, pwd;
     pwd="123";
+    i=0;
     do{
-        cout<<"Introduzca su clave:";
+        cout<<"Introduzca su clave: ";
         cin>>clave;
-    } while (clave!=pwd);
-    cout<<"Bienvenido"<<endl;
+        i=i+1;
+        system("cls");
+    } while ((clave!=pwd)&&(i<3));
+    if (clave!=pwd){
+        cout<<"Superaste los 3 intentos permitidos";
+    } else {
+        cout<<"Bienvenido"<<endl;
+        system("pause");
+    }
     return 0;
 }
